83f1f2de3ef768eac3857b03fc357b8d79678267
Ankit Khanal <ankitkhanal231@gmail.com>
2025-09-20 21:45:28 +0545

    fixes: incorrect indentation in create and update serializer for political party

--- apps/political_party/serializers.py
+++ apps/political_party/serializers.py
@@ -18,21 +18,20 @@ class CreatePoliticalPartySerializer(serializers.ModelSerializer):
             "logo_url",
         ]
 
-    # require all fields from FE, but allow them to be set as black or null as required
-    extra_kwargs = {
-        "name": {"required": True},
-        "description": {"required": True, "allow_blank": False},
-        "abbreviation": {"required": True, "allow_blank": False},
-        "founded_date": {
-            "required": True,
-            "allow_null": False,
-            "allow_blank": False,
-        },
-        "ideology": {"required": True, "allow_blank": False},
-        "hq_location": {"required": True, "allow_blank": False},
-        "website": {"required": True, "allow_blank": True},
-        "logo_url": {"required": True, "allow_blank": False},
-    }
+        # require all fields from FE, but allow them to be set as black or null as required
+        extra_kwargs = {
+            "name": {"required": True},
+            "description": {"required": True, "allow_blank": False},
+            "abbreviation": {"required": True, "allow_blank": False},
+            "founded_date": {
+                "required": True,
+                "allow_null": False,
+            },
+            "ideology": {"required": True, "allow_blank": False},
+            "hq_location": {"required": True, "allow_blank": False},
+            "website": {"required": True, "allow_blank": True},
+            "logo_url": {"required": True, "allow_blank": False},
+        }
 
 
 class UpdatePoliticalPartySerializer(serializers.ModelSerializer):
@@ -49,19 +48,18 @@ class UpdatePoliticalPartySerializer(serializers.ModelSerializer):
             "logo_url",
         ]
 
-    # require all fields from FE, but allow them to be set as black or null as required
-    # NOTE: we'll be using partial=True so the required fields will be optional
-    extra_kwargs = {
-        "name": {"required": True},
-        "description": {"required": True, "allow_blank": False},
-        "abbreviation": {"required": True, "allow_blank": False},
-        "founded_date": {
-            "required": True,
-            "allow_null": False,
-            "allow_blank": False,
-        },
-        "ideology": {"required": True, "allow_blank": False},
-        "hq_location": {"required": True, "allow_blank": False},
-        "website": {"required": True, "allow_blank": True},
-        "logo_url": {"required": True, "allow_blank": False},
-    }
+        # require all fields from FE, but allow them to be set as black or null as required
+        # NOTE: we'll be using partial=True so the required fields will be optional
+        extra_kwargs = {
+            "name": {"required": True},
+            "description": {"required": True, "allow_blank": False},
+            "abbreviation": {"required": True, "allow_blank": False},
+            "founded_date": {
+                "required": True,
+                "allow_null": False,
+            },
+            "ideology": {"required": True, "allow_blank": False},
+            "hq_location": {"required": True, "allow_blank": False},
+            "website": {"required": True, "allow_blank": True},
+            "logo_url": {"required": True, "allow_blank": False},
+        }
